751a3f7c5949299a9317f5256487874426fd7aa3
Bohdan <bogdan.misko85@gmail.com>
2025-02-25 12:48:48 +0200

    Checked entire program running time

--- BenchmarkItemsStore/Program.cs
+++ BenchmarkItemsStore/Program.cs
@@ -16,8 +16,5 @@ namespace BenchmarkItemsStore
 
             Console.WriteLine($"Entire running time: {totalPerformanceTimer.ElapsedMilliseconds} ms.");
         }
-        
-        // Running time: 7001, 7100, 6903, 4836, 5045, 7142, 6977
-        //TODO: Check entire benchmark running time with run preload test async 1 and 5
     }
 }
