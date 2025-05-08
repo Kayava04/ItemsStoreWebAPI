5971afb9efc0e0e146cd90acf5805259afebe697
Bohdan Mysko <103734275+Kayava04@users.noreply.github.com>
2025-05-08 19:45:07 +0300

    Revert "feat: added space (#1)" (#2)
    This reverts commit 6cf9d699c149610cfbc73e417ab73b50d9927b1f.

--- FileToolKit/obj/Debug/net8.0/FileToolKit.AssemblyInfo.cs
+++ FileToolKit/obj/Debug/net8.0/FileToolKit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileToolKit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf6484c272eacf45ba5667b07318cc1cd600656")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd21587b3eb0b66967afe58f06b1bdc04616c33")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileToolKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileToolKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
